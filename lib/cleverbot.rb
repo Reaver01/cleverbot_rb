b31a39d6fb9a6f143581895298919f3d35b5c33e
reaver01 <reaver_01@gmx.com>
2017-02-10 23:00:13 +0100

    change to cleverbot.com api

--- lib/cleverbot.rb
+++ lib/cleverbot.rb
@@ -3,33 +3,22 @@ require 'oj'
 require_relative 'cleverbot_errors'
 
 class Cleverbot
-  # @return [String] The API User for the instance.
-  attr_reader :api_user
-
   # @return [String] The API Key for the instance.
   attr_reader :api_key
 
-  # @return [String] The reference nick for the session.
-  attr_accessor :nick
-
   # Creates a new instance of the Cleverbot.
-  # @param api_user [String] The API user for the Cleverbot API.
   # @param api_key [String] The API key for the Cleverbot API.
-  # @param nick [String] The reference nick. If nil, one will be set automatically through the API.
-  def initialize(api_user, api_key, nick = nil)
-    @api_user = api_user
+  def initialize(api_key)
     @api_key = api_key
-    @nick = nick
     @client = HTTPClient.new
 
     params = {
-      user: @api_user,
       key: @api_key
     }
-    params[:nick] = @nick unless @nick.nil?
-    response = Oj.load(@client.post('https://cleverbot.io/1.0/create', params).body)
-    try_throw(response['status'])
-    @nick = response['nick']
+    response = Oj.load(@client.post('http://cleverbot.com/getreply', params).body)
+    try_throw(response['errorline'])
+
+    @cs = response['cs']
   end
 
   # Sends the bot a message and returns its response.
@@ -37,13 +26,12 @@ class Cleverbot
   # @return [String] The bot's response, or its error message.
   def say(str)
     params = {
-      user: @api_user,
       key: @api_key,
       text: str,
-      nick: @nick
+      cs: @cs
     }
-    response = Oj.load(@client.post('https://cleverbot.io/1.0/ask', params).body)
-    try_throw(response['status'])
+    response = Oj.load(@client.post('http://cleverbot.com/getreply', params).body)
+    try_throw(response['errorline'])
 
     response['response']
   end
@@ -54,12 +42,12 @@ class Cleverbot
   # @param status [String] The status value from the API
   # @raise [IncorrectCredentialsError] If the api_user and api_key are incorrect.
   # @raise [DuplicatedReferenceNamesError] If the reference name is already in use by the instance.
-  def try_throw(status)
-    case status
+  def try_throw(errorline)
+    case errorline
     when 'Error: API credentials incorrect' then fail Cleverbot::Errors::IncorrectCredentialsError
     when 'Error: reference name already exists' then fail Cleverbot::Errors::DuplicatedReferenceNamesError
     when 'success' then return
-    else fail "#{status} UNRECOGNIZED ERROR! PLEASE REPORT TO CLEVERBOT RUBY ISSUE TRACKER."
+    else fail "#{errorline} UNRECOGNIZED ERROR! PLEASE REPORT TO CLEVERBOT RUBY ISSUE TRACKER."
     end
   end
 end
